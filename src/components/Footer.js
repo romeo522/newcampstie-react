8b174cadf003ce7093eaa8a62c2de4d2dfd6c3c0
romeo <romeorangeljr@gmail.com>
2022-10-29 12:26:29 -0500

    React | Week 1 | Workshop social icons fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,19 +12,19 @@ const Footer = () => {
 
           <Col xs="6" sm="3" className="text-center">
             <h5>Social</h5>
-            <a className="btn btn-instagram" href="http://instagram.com/" >
+            <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/" >
               <i className="fa fa-instagram" />
             </a>
             {" "}
-            <a className="btn btn-facebook" href="http://www.facebook.com/" > 
+            <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/" > 
               <i className="fa fa-facebook" />
             </a>
             {" "}
-            <a className="btn btn-twitter" href="http://twitter.com/" >
+            <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/" >
               <i className="fa fa-twitter" />
             </a>
             {" "}
-            <a className="btn btn-google" href="http://youtube.com/" >
+            <a className="btn btn-social-icon btn-google" href="http://youtube.com/" >
               <i className="fa fa-youtube" />
             </a>
           </Col>
