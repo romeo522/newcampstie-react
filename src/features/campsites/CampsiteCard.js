3ff1b4b98c337c65a3096f85f6af00b20497ae32
romeo <romeorangeljr@gmail.com>
2022-11-01 13:48:40 -0500

    CampsitesDirectoryPage & CampsiteDetail

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -3,12 +3,12 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 const CampsiteCard = ({ campsite }) => {
   const { image, name } = campsite;
   return (
-      <Card>
-          <CardImg width='100%' src={image} alt={name} />
-          <CardImgOverlay>
-              <CardTitle>{name}</CardTitle>
-          </CardImgOverlay>
-      </Card>
+    <Card>
+      <CardImg width='100%' src={image} alt={name} />
+      <CardImgOverlay>
+        <CardTitle>{name}</CardTitle>
+      </CardImgOverlay>
+    </Card>
   );
 };
 
