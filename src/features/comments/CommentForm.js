7a86d561a4568736bd4e5e07a8368546237a8835
romeo <romeorangeljr@gmail.com>
2022-11-11 11:19:12 -0600

    React | Week 3 | Workshop Task 1

---
+++ src/features/comments/CommentForm.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody } from "reactstrap";
+
+export const CommentForm = ({ campsiteId }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  return (
+    <>
+      <Button outline onClick={() => setModalOpen(true)}>
+        <i className="fa fa-pencil fa-lg" />
+        Add Comment
+      </Button>
+
+      <Modal isOpen={modalOpen}>
+        <ModalHeader toggle={() => setModalOpen(false)}>
+          Add Comment
+        </ModalHeader>
+        <ModalBody>
+          campsite: {campsiteId}
+        </ModalBody>
+      </Modal>
+    </>
+  );
+};
