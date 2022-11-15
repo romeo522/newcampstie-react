8f1ccd79690f6e08b3a09a3ae887d573c2f1c99c
romeo <romeorangeljr@gmail.com>
2022-11-15 17:54:57 -0600

    React | Week 4 | Workshop Task 3 (Final)

--- src/features/users/UserLoginForm.js
+++ src/features/users/UserLoginForm.js
@@ -2,8 +2,9 @@ import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setCurrentUser, selectCurrentUser } from './userSlice';
 import { Modal, ModalHeader, ModalBody, FormGroup, Label, Button } from 'reactstrap';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import defaultAvatar from '../../app/assets/img/unicorn.png';
+import { validateUserLoginForm } from '../../utils/validateUserLoginForm';
 
 export const UserLoginForm = () => {
   const [loginModalOpen, setLoginModalOpen] = useState(false);
@@ -42,15 +43,21 @@ export const UserLoginForm = () => {
           Login
         </ModalHeader>
         <ModalBody>
-          <Formik initialValues={{ username:'', password:'' }} onSubmit={handleLogin}>
+          <Formik initialValues={{ username:'', password:'' }} onSubmit={handleLogin} validate={validateUserLoginForm}>
             <Form>
               <FormGroup>
                 <Label htmlFor='username'>Username</Label>
                 <Field id='username' name='username' placeholder='Username' className='form-control' />
+                <ErrorMessage name='username'>
+                  {(msg) => <p className='text-danger'>{msg}</p>}
+                </ErrorMessage>
               </FormGroup>
               <FormGroup>
                 <Label htmlFor='password'>Password</Label>
                 <Field id='password' name='password' placeholder='Password' className='form-control' />
+                <ErrorMessage name='password'>
+                  {(msg) => <p className='text-danger'>{msg}</p>}
+                </ErrorMessage>
               </FormGroup>
               <Button type='submit' color='primary'>Login</Button>
             </Form>
