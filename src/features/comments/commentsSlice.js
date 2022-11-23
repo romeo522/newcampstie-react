1837132a4c22d63c4f618e5305d0ef3d36fe206c
romeo <romeorangeljr@gmail.com>
2022-11-23 11:58:40 -0600

    React | Week 5 | Workshop Task 1

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,8 +1,23 @@
-import { COMMENTS } from "../../app/shared/COMMENTS";
-import { createSlice } from '@reduxjs/toolkit';
+// import { COMMENTS } from "../../app/shared/COMMENTS";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { baseUrl } from "../../app/shared/baseUrl";
+
+export const fetchComments = createAsyncThunk(
+  "comments/fetchComments",
+  async () => {
+    const response = await fetch(baseUrl + "comments");
+    if (!response.ok) {
+      return Promise.reject("Unable to fetch, status: " + response.status);
+    }
+    const data = await response.json();
+    return data;
+  }
+);
 
 const initialState = {
-  commentsArray: COMMENTS
+  commentsArray: [],
+  isLoading: true,
+  errMsg: "",
 };
 
 const commentsSlice = createSlice({
@@ -18,7 +33,21 @@ const commentsSlice = createSlice({
       };
       state.commentsArray.push(newComment);
     }
-  }
+  },
+  extraReducers: {
+    [fetchComments.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchComments.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.errMsg = "";
+      state.commentsArray = action.payload;
+    },
+    [fetchComments.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.errMsg = action.error ? action.error.message : "Fetch failed";
+    },
+  },
 });
 
 export const commentsReducer = commentsSlice.reducer;
