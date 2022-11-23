b8261968b3d3c14b731ffe834e88ff45589a0132
romeo <romeorangeljr@gmail.com>
2022-11-23 15:07:47 -0600

    React | Week 5 | Workshop Task 3 (Final)

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -14,6 +14,22 @@ export const fetchComments = createAsyncThunk(
   }
 );
 
+export const postComment = createAsyncThunk(
+  "comments/postComment",
+  async (comment, { dispatch }) => {
+    const response = await fetch(baseUrl + "comments", {
+      method: "POST",
+      body: JSON.stringify(comment),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (!response.ok) {
+      return Promise.reject(response.status);
+    }
+    const data = await response.json();
+    dispatch(addComment(data));
+  }
+);
+
 const initialState = {
   commentsArray: [],
   isLoading: true,
@@ -46,6 +62,13 @@ const commentsSlice = createSlice({
     [fetchComments.rejected]: (state, action) => {
       state.isLoading = false;
       state.errMsg = action.error ? action.error.message : "Fetch failed";
+    },
+    [postComment.rejected]: (state, action) => {
+      state.isLoading = false;
+      alert(
+        "Your comment could not be posted\n Error: " + 
+          (action.error ? action.error.message : "fetch failed.")
+      );
     },
   },
 });
